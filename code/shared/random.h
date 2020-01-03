b4d8e03383a26ec0ffcca25b6ec53b7a39bbae51
feacur <feacur@gmail.com>
2020-01-03 18:40:38 +0300

    fix launch and random

--- code/shared/random.h
+++ code/shared/random.h
@@ -10,15 +10,15 @@
 
 inline uint32 hash_xorshift32(uint32 * state) {
 	uint32 x = *state;
-	x ^= x << 13; // hash
-	x ^= x >> 17; // hash
-	x ^= x << 15; // hash
+	x ^= x << 13U; // hash
+	x ^= x >> 17U; // hash
+	x ^= x << 15U; // hash
 	return (*state = x);
 }
 
 inline uint32 hash_lehmer32(uint32 * state) {
-	static uint64 const multiplier = 0x10a860c1; // 0x0000bc8f
-	static uint64 const modulo     = 0xfffffffb; // 0x7fffffff
+	static uint64 const multiplier = 0x10a860c1UL; // 0x0000bc8fUL
+	static uint64 const modulo     = 0xfffffffbUL; // 0x7fffffffUL
 	uint64 x = *state;
 	x = (x * multiplier) % modulo; // hash
 	return (*state = (uint32)x);
@@ -26,16 +26,16 @@ inline uint32 hash_lehmer32(uint32 * state) {
 
 inline float hash_01(uint32 * state) {
 	union { uint32 x; float xf; };  // local unionized values
-	x = (*state = *state * 16807);  // hash
-	x = (x >> 9) | 0x3f800000; // clamp to [1 .. 2) * (2^0)
+	x = (*state = *state * 16807U); // hash
+	x = (x >> 9) | 0x3f800000U;     // clamp to [1 .. 2) * (2^0)
 	return xf - 1;                  // return [1 .. 2) - 1
 }
 
 inline float hash_radius01(uint32 * state) {
-	union { uint32 x; float xf; }; // local unionized values
-	x = (*state = *state * 16807); // hash
-	x = (x >> 9) | 0x40000000;     // clamp to [1 .. 2) * (2^1)
-	return xf - 3;                 // return [2 .. 4) - 3
+	union { uint32 x; float xf; };  // local unionized values
+	x = (*state = *state * 16807U); // hash
+	x = (x >> 9) | 0x40000000U;     // clamp to [1 .. 2) * (2^1)
+	return xf - 3;                  // return [2 .. 4) - 3
 }
 
 //
