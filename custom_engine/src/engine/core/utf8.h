47d1e118a4d4091bd3e413c054a79a84ae7d764f
feacur <feacur@gmail.com>
2020-01-22 20:34:50 +0300

    sync

--- custom_engine/src/engine/core/utf8.h
+++ custom_engine/src/engine/core/utf8.h
@@ -6,8 +6,8 @@ namespace unicode {
 
 typedef char const * utf8;
 
-u32 decode(utf8 value);
-utf8 next(utf8 value);
-u32 count(utf8 value);
+u32 decode(utf8 buffer);
+utf8 next(utf8 buffer);
+u32 count(utf8 buffer);
 
 }}
--- custom_engine/src/engine/internal/utf8.cpp
+++ custom_engine/src/engine/internal/utf8.cpp
@@ -9,61 +9,61 @@ namespace custom {
 namespace unicode {
 
 struct encoding {
-	u8 signature, mask, lead, continuation;
+	u8 signature, mask, leading, continuation;
 };
 
 static encoding const table[] = {
-//    signature   mask        lead. byte  cont. byte
+	// signature  mask        lead. byte  cont. byte
 	{ 0b11110000, 0b11111000, 0b00000111, 0b00111111, }, // 21 bit  = 3 + 6 + 6 + 6
 	{ 0b11100000, 0b11110000, 0b00001111, 0b00111111, }, // 16 bits = 4 + 6 + 6 __0
 	{ 0b11000000, 0b11100000, 0b00011111, 0b00111111, }, // 11 bits = 5 + 6 __0 __0
 	{ 0b00000000, 0b10000000, 0b01111111, 0b00000000, }, //  7 bits = 7 __0 __0 __0
 };
 
-u32 decode(utf8 value) {
-	if ((value[0] & table[0].mask) == table[0].signature) { // 4 bytes
-		return (value[0] & table[0].lead)         << 18
-		     | (value[1] & table[0].continuation) << 12
-		     | (value[2] & table[0].continuation) <<  6
-		     | (value[3] & table[0].continuation);
+u32 decode(utf8 buffer) {
+	if ((buffer[0] & table[0].mask) == table[0].signature) { // 4 bytes
+		return (buffer[0] & table[0].leading)      << 18
+		     | (buffer[1] & table[0].continuation) << 12
+		     | (buffer[2] & table[0].continuation) <<  6
+		     | (buffer[3] & table[0].continuation);
 	}
-	if ((value[0] & table[1].mask) == table[1].signature) { // 3 bytes
-		return (value[0] & table[1].lead)         << 12
-		     | (value[1] & table[1].continuation) <<  6
-		     | (value[2] & table[1].continuation);
+	if ((buffer[0] & table[1].mask) == table[1].signature) { // 3 bytes
+		return (buffer[0] & table[1].leading)      << 12
+		     | (buffer[1] & table[1].continuation) <<  6
+		     | (buffer[2] & table[1].continuation);
 	}
-	if ((value[0] & table[2].mask) == table[2].signature) { // 2 bytes
-		return (value[0] & table[2].lead)         <<  6
-		     | (value[2] & table[2].continuation);
+	if ((buffer[0] & table[2].mask) == table[2].signature) { // 2 bytes
+		return (buffer[0] & table[2].leading)      <<  6
+		     | (buffer[2] & table[2].continuation);
 	}
-	if ((value[0] & table[3].mask) == table[3].signature) { // 1 byte
-	 	return (value[0] & table[3].lead);
+	if ((buffer[0] & table[3].mask) == table[3].signature) { // 1 byte
+	 	return (buffer[0] & table[3].leading);
 	}
 	CUSTOM_ASSERT(false, "trying to decode a single continuation byte");
 	return 0;
 }
 
-utf8 next(utf8 value) {
-	if ((value[0] & table[0].mask) == table[0].signature) {
-		return value + 4;
+utf8 next(utf8 buffer) {
+	if ((buffer[0] & table[0].mask) == table[0].signature) {
+		return buffer + 4;
 	}
-	if ((value[0] & table[1].mask) == table[1].signature) {
-		return value + 3;
+	if ((buffer[0] & table[1].mask) == table[1].signature) {
+		return buffer + 3;
 	}
-	if ((value[0] & table[2].mask) == table[2].signature) {
-		return value + 2;
+	if ((buffer[0] & table[2].mask) == table[2].signature) {
+		return buffer + 2;
 	}
-	if ((value[0] & table[3].mask) == table[3].signature) {
-		return value + 1;
+	if ((buffer[0] & table[3].mask) == table[3].signature) {
+		return buffer + 1;
 	}
-	return value;
+	return buffer;
 }
 
-u32 count(utf8 value) {
+u32 count(utf8 buffer) {
 	u32 count = 0;
-	while (*value) {
+	while (*buffer) {
 		++count;
-		value = next(value);
+		buffer = next(buffer);
 	}
 	return count;
 }
