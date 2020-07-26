6c2a5c329252442d491adcc64c1e25997ce67d2d
feacur <feacur@gmail.com>
2020-07-26 12:44:30 +0300

    load file buffer directly

--- custom_engine/src/engine/internal/loader_lua.cpp
+++ custom_engine/src/engine/internal/loader_lua.cpp
@@ -11,6 +11,11 @@
 namespace custom {
 namespace loader {
 
+// @Note: alternative to `luaL_dostring(L, (cstring)file.data)`
+#define CUSTOM_LOAD()\
+	luaL_loadbufferx(L, (cstring)file.data, file.count, path, NULL)\
+	|| lua_pcall(L, 0, LUA_MULTRET, 0)\
+
 void script(lua_State * L, u32 asset_id) {
 	// if (has_script(asset_id)) { return; }
 	if (asset_id >= asset::script::count) { return; }
@@ -18,12 +23,12 @@ void script(lua_State * L, u32 asset_id) {
 
 	Array<u8> file; file::read(path, file);
 	if (file.count != file.capacity) { return; }
-	file.push('\0');
 
-	if (luaL_dostring(L, (cstring)file.data) != LUA_OK) {
+	if (CUSTOM_LOAD() != LUA_OK) {
 		CUSTOM_ERROR("lua: '%s'", lua_tostring(L, -1));
 		lua_pop(L, 1);
 	}
 }
+#undef CUSTOM_LOAD
 
 }}
