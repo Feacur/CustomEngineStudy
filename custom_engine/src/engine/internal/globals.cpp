dcdf98c084cbf55a86fea16a16e7c9b27b3b8eff
feacur <feacur@gmail.com>
2020-08-26 23:36:02 +0300

    sync

--- custom_engine/src/engine/internal/globals.cpp
+++ custom_engine/src/engine/internal/globals.cpp
@@ -1,11 +1,7 @@
 #include "custom_pch.h"
 
-#include "engine/core/code.h"
 #include "engine/core/collection_types.h"
-#include "engine/debug/log.h"
 #include "engine/api/rendering_settings.h"
-
-#include <string.h>
 
 namespace custom {
 namespace system {
@@ -27,38 +23,4 @@ namespace custom {
 	Array<cstring> component_names;
 	Array<cstring> asset_names;
 	Array<cstring> uniform_names;
-}
-
-namespace custom {
-	// @Todo: store strings
-	static Array<char> todo_strings;
-	static Array<u32> todo_strings_lengths;
-
-	cstring get(u32 id) {
-		u32 string_offset = 0;
-		for (u32 i = 0; i < id; ++i) {
-			string_offset += todo_strings_lengths[i];
-		}
-		return &todo_strings.data[string_offset];
-	}
-
-	u32 get_or_add_id(cstring data, u32 length) {
-		if (length == custom::empty_index) {
-			length = (u32)strlen(data);
-		}
-
-		u32 string_offset = 0;
-		for (u32 i = 0; i < todo_strings_lengths.count; ++i) {
-			u32 string_length = todo_strings_lengths[i];
-			cstring name = &todo_strings[string_offset];
-			if (strncmp(data, name, length) == 0) { return i; }
-			string_offset += string_length;
-		}
-
-		u32 id = todo_strings_lengths.count;
-		todo_strings_lengths.push(length + 1);
-		todo_strings.push_range(data, length);
-		todo_strings.push('\0');
-		return id;
-	}
 }
