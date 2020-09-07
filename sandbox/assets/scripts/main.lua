76f2c3738518a6d06266f81ab280088cf0cc1032
feacur <feacur@gmail.com>
2020-09-07 19:22:59 +0300

    allow quick scene reload

--- sandbox/assets/scripts/main.lua
+++ sandbox/assets/scripts/main.lua
@@ -1,12 +1,16 @@
+local function local_init()
+	Asset.reset_system(Prefab_Asset.type)
+	Entity.reset_system()
+	
+	Asset.add(Lua_Asset.type, "assets/scripts/physics test.lua")
+	global_init_physics_test()
+	
+	-- Asset.add(Lua_Asset.type, "assets/scripts/prefabs test.lua")
+	-- global_init_prefab_test()
+end
+
 -- executed on load
-Asset.reset_system(Prefab_Asset.type)
-Entity.reset_system()
-
-Asset.add(Lua_Asset.type, "assets/scripts/physics test.lua")
-global_init_physics_test()
-
--- Asset.add(Lua_Asset.type, "assets/scripts/prefabs test.lua")
--- global_init_prefab_test()
+local_init()
 
 -- executed on the app start
 function global_init()
@@ -16,4 +20,8 @@ end
 function global_update()
 	-- global_update_physics_test()
 	-- global_update_prefab_test()
+	
+	if Input.get_key(Key_Code.Shift) and Input.get_key(Key_Code.F5) then
+		local_init()
+	end
 end
